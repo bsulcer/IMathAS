0b3204918573e96b47669463c5c0bbd5b56aea49
Brian Sulcer <bsulcer@pobox.com>
2015-04-07 12:18:57 -0700

    Allow override of domain-level cookie via config

--- includes/utils.php
+++ includes/utils.php
@@ -1,13 +1,25 @@
 <?php
+
+function utils_get_cfg($section, $name, $default = null) {
+	global $CFG;
+	if (isset($CFG[$section]) && isset($CFG[$section][$name])) {
+		return $CFG[$section][$name];
+	} else {
+		return $default;
+	}
+}
+
 function utils_start_session() {
+	global $sessionpath;
 	if (isset($sessionpath) && $sessionpath!='') { session_save_path($sessionpath);}
 	ini_set('session.gc_maxlifetime',86400);
 	ini_set('auto_detect_line_endings',true);
  
-	if ($_SERVER['HTTP_HOST'] != 'localhost') {
-		session_set_cookie_params(0, '/', '.'.implode('.',array_slice(explode('.',$_SERVER['HTTP_HOST']),isset($CFG['GEN']['domainlevel'])?$CFG['GEN']['domainlevel']:-2)));
+	$domainlevel = utils_get_cfg('GEN', 'domainlevel', -2);
+	if ($_SERVER['HTTP_HOST'] != 'localhost' && $domainlevel != 0) {
+		session_set_cookie_params(0, '/', '.'.implode('.',array_slice(explode('.',$_SERVER['HTTP_HOST']),$domainlevel)));
  	}
- 
+
 	session_start();
 	return session_id();
 }

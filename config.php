1f21cf12ce289ba2a66f7b813972a8201e335a73
Brian Sulcer <bsulcer@pobox.com>
2015-07-06 21:35:09 -0700

    Correct typo

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
 // emulation of original config.php
-require_once('config_real.php');
+require_once('config_local.php');
 require('includes/init_legacy_mysql.php');
 require('includes/init_mangle_request.php');
